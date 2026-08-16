9daad8545a02f8e5e5c99c3c97cffc86136fb720
Muhammad Hozaif <mhozaif@bitsol.ai>
2026-08-16 19:01:29 +0500

    added the keyboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { languages } from "./languages.js";
 export default function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+
   return (
-    <>
-      <main>
-        <header>
-          <h1>Assembly: Endgame</h1>
-          <p>
-            Guess the word within 8 attempts to keep the programming world safe
-            from Assembly!
-          </p>
-        </header>
-        <section className="game-status">
-          <h2>You win!</h2>
-          <p>Well done! 🎉</p>
-        </section>
-      </main>
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+    </main>
   );
 }
